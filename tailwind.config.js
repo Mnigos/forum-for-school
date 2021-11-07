d46628dcf43aa5dfba39752f90edc5a2470968f3
Mnigos <makowskiigor@gmail.com>
2021-11-07 11:34:15 +0100

    chore(tailwind): add purge components, layouts and pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@
 module.exports = {
   mode: 'jit',
   purge: [
-    // ...
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}',
     './node_modules/@vechaiui/**/*.{js,ts,jsx,tsx}', // path to vechaiui
   ],
   darkMode: 'class', // or 'media' or 'class'
