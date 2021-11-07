8411095acf3eecfc5654298eea5289ae02ed8dbd
Mnigos <makowskiigor@gmail.com>
2021-11-07 15:45:17 +0100

    refactor(_app): delete unused button

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,10 @@
 import '../styles/globals.css'
-import { VechaiProvider, Button } from '@vechaiui/react'
+import { VechaiProvider } from '@vechaiui/react'
 import { AppProps } from 'next/dist/shared/lib/router/router'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <VechaiProvider>
-      <Button>Hello Vechai</Button>
       <Component {...pageProps} />
     </VechaiProvider>
   )
