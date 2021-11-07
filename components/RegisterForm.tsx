c9a754bea2308e6528257d50af324f0f8bfd13b3
Mnigos <makowskiigor@gmail.com>
2021-11-07 17:09:24 +0100

    feat(auth): create register page

---
+++ components/RegisterForm.tsx
@@ -0,0 +1,120 @@
+import React, { ChangeEvent, FormEvent, useState } from 'react'
+import { Button } from '@vechaiui/button'
+
+import { PasswordInput } from './PasswordInput'
+import UsernameInput from './UsernameInput'
+
+import { capitalize } from '~/utils/capitalize'
+
+interface RegisterState {
+  username: string
+  password: string
+  repeatedPassword: string
+}
+
+const initialRegisterState: RegisterState = {
+  username: '',
+  password: '',
+  repeatedPassword: '',
+}
+
+export default function RegisterForm() {
+  const [formValues, setFormValues] = useState(initialRegisterState)
+  const [errors, setError] = useState(initialRegisterState)
+
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target
+
+    if (!value)
+      setError({
+        ...errors,
+        [name]: capitalize(`${name} cannot be empty`),
+      })
+    else
+      setError({
+        ...errors,
+        [name]: '',
+      })
+
+    if (
+      (['password', 'repeatedPassword'].includes(name) &&
+        value !== formValues.password) ||
+      value !== formValues.repeatedPassword
+    )
+      setError({
+        ...errors,
+        repeatedPassword: "Passwords don't match",
+      })
+    else
+      setError({
+        ...errors,
+        repeatedPassword: '',
+      })
+
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    })
+  }
+
+  function validate(values: RegisterState): boolean {
+    const foundedErrors: RegisterState = {
+      username: '',
+      password: '',
+      repeatedPassword: '',
+    }
+
+    if (!values.username) foundedErrors.username = 'Username cannot be empty'
+    if (!values.password) foundedErrors.password = 'Password cannot be empty'
+    if (values.repeatedPassword !== values.password)
+      foundedErrors.repeatedPassword = "Passwords don't match"
+
+    const { username, password, repeatedPassword } = foundedErrors
+
+    setError({
+      username,
+      password,
+      repeatedPassword,
+    })
+
+    return !password && !username && !repeatedPassword ? true : false
+  }
+
+  function handleSubmitLogin(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    // eslint-disable-next-line no-console
+    if (validate(formValues)) console.log('registered!')
+  }
+
+  return (
+    <form
+      className="flex flex-col max-w-xs gap-4 p-8 bg-gray-100 rounded-xl"
+      onSubmit={handleSubmitLogin}
+    >
+      <UsernameInput
+        error={errors.username}
+        value={formValues.username}
+        handleInputChange={handleInputChange}
+      />
+
+      <PasswordInput
+        error={errors.password}
+        value={formValues.password}
+        handleInputChange={handleInputChange}
+      />
+
+      <PasswordInput
+        error={errors.repeatedPassword}
+        name="repeatedPassword"
+        label="Repeat Password"
+        value={formValues.repeatedPassword}
+        handleInputChange={handleInputChange}
+      />
+
+      <Button className="mt-4 cursor-pointer" type="submit">
+        Register
+      </Button>
+    </form>
+  )
+}
---
+++ pages/auth/register.tsx
@@ -0,0 +1,9 @@
+import RegisterForm from '~/components/RegisterForm'
+
+export default function Login() {
+  return (
+    <div className="flex justify-center">
+      <RegisterForm />
+    </div>
+  )
+}
