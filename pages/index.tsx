43dbc56736d2ba28bb966ab602580dc4d0922198
Mnigos <makowskiigor@gmail.com>
2021-11-08 20:13:48 +0100

    feat(posts): create PostCard component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,39 @@
 import React from 'react'
 
 import CreatePostCard from '~/components/posts/CreatePostCard'
+import PostCard from '~/components/posts/PostCard'
 import { DialogProvider } from '~/providers/DialogProvider'
+
+const loremIpsum =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+
+const cards = [
+  {
+    title: 'First Card',
+    body: loremIpsum,
+  },
+  {
+    title: 'Second Card',
+    body: loremIpsum,
+  },
+  {
+    title: 'Third Card',
+    body: loremIpsum,
+  },
+]
 
 export default function Home() {
   return (
-    <div className="flex min-h-[80vh] justify-center items-center">
+    <div className="flex flex-col items-center justify-center min-h-screen gap-20">
       <DialogProvider>
         <CreatePostCard />
       </DialogProvider>
+
+      <div className="flex flex-col gap-5">
+        {cards.map(({ title, body }, index) => (
+          <PostCard title={title} body={body} key={index} />
+        ))}
+      </div>
     </div>
   )
 }
