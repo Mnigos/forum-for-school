209f2ffd1d505395b992e0db0174b12e8ade5aa4
Mnigos <makowskiigor@gmail.com>
2021-11-19 12:20:05 +0100

    feat: create Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => (
+  <div className="w-screen flex justify-center py-5 bg-[#0D9488]">
+    <p className="text-white">
+      Copyright &copy; {new Date().getFullYear} 4Devs | Igor Makowski
+    </p>
+  </div>
+)
+
+export default Footer
