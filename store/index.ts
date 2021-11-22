d1e643cabc72398e9a971bf72bac7892c9fa1a6c
Mnigos <makowskiigor@gmail.com>
2021-11-22 19:54:14 +0100

    test

--- store/index.ts
+++ store/index.ts
@@ -1,9 +1,15 @@
 import { applyMiddleware, createStore } from 'redux'
 import thunk from 'redux-thunk'
+import {
+  TypedUseSelectorHook,
+  useSelector as useGenericSelector,
+} from 'react-redux'
 
 import reducers from './reducers'
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk))
+
+export const useSelector: TypedUseSelectorHook<RootState> = useGenericSelector
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
